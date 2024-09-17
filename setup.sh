4556e841900b8097c00b09952733077c484bbe7b
Bruno Santos <brunolsantos152@gmail.com>
2024-09-17 09:25:28 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -187,10 +187,12 @@ server {
     location / {
         proxy_pass http://localhost:8080;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection keep-alive;
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
 }
 EOL
