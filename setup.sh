149c56a2a08b2e781ddcb96c5f203ef93a11b5e9
Bruno Santos <brunolsantos152@gmail.com>
2024-09-23 21:32:00 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -130,7 +130,7 @@ After=network.target ollama.service
 Type=simple
 User=$APP_USER
 WorkingDirectory=$APP_DIR
-ExecStart=$APP_DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:8080 --timeout 600 app:app
+ExecStart=$APP_DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:8080 --timeout 6000 app:app
 Environment=API_KEY=$API_KEY
 Restart=always
 LimitNOFILE=65536
